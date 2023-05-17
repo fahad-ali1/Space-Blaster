38b5557c75dd6221d12954ee19b9551d922edab1
Fahad <fahadali@live.ca>
2023-05-16 23:28:32 -0600

    Better screen fit and other updates

--- source/constant.py
+++ source/constant.py
@@ -1,17 +1,23 @@
+import arcade
 # Constants for the screen and title
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 700
+width, height = arcade.window_commands.get_display_size()
+SCREEN_WIDTH = width-200
+SCREEN_HEIGHT = height-200
 SCREEN_TITLE = "Space Blaster"
 
 # Constants used to scale the sprites
-CHARACTER_SCALING = 1
+SHIP_SCALING = 1
 
 # Movement speed of player, in pixels per frame
-PLAYER_MOVEMENT_SPEED = 15
+PLAYER_SHIP_MAX_SPEED = 15
+PLAYER_ACCEL = 0.1
+PLAYER_SHIP_DRAG = 0.02
 
 # Movement speed of bullet
 SHIP_BULLET_SPEED = 20
 BULLET_SCALING = 1
+MAX_180_BULLETS = 2
+MAX_MOUSE_BULLETS = 2
 
 # Rock Scaling
 ROCK_SCALING = 1
