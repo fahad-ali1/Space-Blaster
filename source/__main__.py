29afa2a5cfb6781b6109fd834c0ce6d7fa801cf9
Fahad <fahadali@live.ca>
2023-05-31 16:48:59 -0600

    Update __main__.py

--- source/__main__.py
+++ source/__main__.py
@@ -1,6 +1,5 @@
 """
-A space 2D block blasasasasting game with ship upgrades, special game features and 
-more!
+A 2D space blasting game
 
 Images and sounds from https://kenney.nl and google images
 """
@@ -9,7 +8,7 @@ from game_screen import *
 
 def main():
     """Main function"""
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Blaster : Main Menu")
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Blaster")
     main_menu = MenuView()
     window.show_view(main_menu)
     arcade.run()
